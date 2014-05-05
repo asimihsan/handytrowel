a71b36e565fc9af63ad9c14873436d879432768e
Asim Ihsan <angry.blade5619@ihsan.io>
2014-05-05 23:52:58 +0100

    network/TestHTMLFetcher - add AGPL license header.

--- src/test/java/com/asimihsan/handytrowel/network/TestHTMLFetcher.java
+++ src/test/java/com/asimihsan/handytrowel/network/TestHTMLFetcher.java
@@ -1,3 +1,26 @@
+/** ========================================================================
+  * handytrowel: src/test/java/network/TestHTMLFetcher.java
+  * End-to-end tests for HTMLFetcher.
+  * ========================================================================
+  * Copyright (c) 2014, Asim Ihsan, All rights reserved.
+  * <http://www.asimihsan.com>
+  * https://github.com/asimihsan/handytrowel/blob/master/LICENSE
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Affero General Public License as published
+  * by the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Affero General Public License for more details.
+  *
+  * You should have received a copy of the GNU Affero General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  * ========================================================================
+  */
+
 package com.asimihsan.handytrowel.network;
 
 import static org.junit.Assert.*;
